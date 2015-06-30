f37e38742737ba53c328d8becbdc8e4e07f44f17
Craig Lonsdale <craig@meerkats.com.au>
2015-06-30 14:49:13 +0800

    Add in the index file and package file Updated documentation with usage

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # gulp-svgrollout
 Rollout multiple single color SVG icons in multiple colors.
+
+# Usage
+```
+var svgrollout = require(gulp-svgrollout);
+var target = '#000000';
+var replace_colours = {
+    'colours': [
+        { colour: 'white', value: '#ffffff' },
+        { colour: 'black', value: '#182124' },
+        { colour: 'yellow', value: '#efbd46' },
+        { colour: 'red', value: [
+            { colour:'flat', value:'#f5644c' },
+            { colour:'vibrant', value:'#f54533' }
+        ]},
+        { colour: 'grey', value: [
+            { colour:'30', value:'#b9bcbd'},
+            { colour:'20', value:'#ececec'},
+            { colour:'10', value:'#f5f5f5'}
+        ]}
+    ]};
+
+gulp.task('rollouticons', function (callback) {
+    return gulp.src(config.files.icons).
+        .pipe(svgrollout(target, replace_colours, config.dir.icons));
+};
+```
+For each of the target passed in a new svg file will be created.
+An input of `input.svg` in this case will some out as:
+- input-white.svg
+- input-black.svg
+- input-yellow.svg
+- input-red-flat.svg
+- input-red-vibrant.svg
+- input-grey-10.svg
+- input-grey-20.svg
+- input-grey-30.svg
---
+++ index.js
@@ -0,0 +1,94 @@
+var _ = require('underscore');
+var async = require('async');
+var fs = require('fs');
+var path = require('path');
+var through = require('through2');
+
+/**
+ * Builds a collection or colours from a JSON object of Colour : Value
+ */
+function buildColourValueArray(colours, next) {
+    function processColour(colour, parent_colour, next) {
+        if (_.isFunction(parent_colour)) {
+            next = parent_colour;
+            parent_colour = null;
+        }
+        async.each(colour.value, function (item, callback) {
+            if (_.isArray(colour.value)) {
+                return processColour(item, colour.colour, next);
+            }
+            return callback();
+        }, function (err) {
+            if (err) {
+                next(err);
+            }
+            var colour_name = colour.colour;
+            if (parent_colour) {
+                colour_name = [parent_colour, '-', colour.colour].join('');
+            }
+            if (!_.isArray(colour.value)) {
+                return next(null, colour_name, colour.value);
+            };
+        });
+    }
+
+    return async.reduce(colours, [], function (colour_list, item, callback) {
+        processColour(item, function (err, colour_name, colour_value) {
+            if (err) {
+                callback(err);
+            }
+            colour_list.push({
+                colour: colour_name,
+                value: colour_value
+            });
+        });
+        callback(null, colour_list);
+    }, next);
+}
+
+/**
+ * Helper method that breaks apart a file path into more usable components.
+ */
+function parsePath(file_path) {
+    var extname = path.extname(file_path);
+    return {
+        dirname: path.dirname(file_path),
+        basename: path.basename(file_path, extname),
+        extname: extname
+    };
+}
+
+/**
+ * Replace the target_colour with replacement_colour in the buffer chunk
+ */
+function replaceBuffered(file, target_colour, replacement_colour) {
+    var chunks = String(file.contents).split(target_colour);
+    var result = chunks.join(replacement_colour);
+    return new Buffer(result);
+}
+
+/**
+ * Creates a new file for each of the repleacement colours,
+ * replacing the target colour with the replacement colour in the svg.
+ * @param {string} Colour you want to replace.
+ * @param {Array} Colours that you want to replace the target with {colour: colour_name, value: colour_value}.
+ * @param {string} The location where the files are to be written to.
+ */
+module.exports = function (target_colour, replacement_colours, output_directory) {
+    return through.obj(function (file, enc, callback) {
+        buildColourValueArray(replacement_colours, function (err, results) {
+            var parsedFilePath = parsePath(file.relative);
+            var write_path = path.join(__dirname, output_directory, parsedFilePath.dirname);
+            if(!fs.existsSync(write_path)) {
+                fs.mkdirSync(write_path);
+            }
+            async.each(results, function (item, callback) {
+                var write_file_path = path.join(write_path, parsedFilePath.basename + '-' + item.colour + parsedFilePath.extname);
+                if (file.isBuffer()) {
+                    fs.writeFile(write_file_path, replaceBuffered(file, target_colour, item.value), callback);
+                }
+            }, callback);
+        });
+    });
+});
+
